4c5b3744563fd877c06549797f93c9bb44881b0c
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-01-05 16:13:12 +0300

    Minor cleanup

--- cmake/modules/MacOsEnvironment.cmake
+++ cmake/modules/MacOsEnvironment.cmake
@@ -74,6 +74,5 @@ if (CMAKE_HOST_SYSTEM_NAME MATCHES "Darwin")
 	set(CMAKE_CXX_ARCHIVE_CREATE "<CMAKE_AR> Scr <TARGET> <LINK_FLAGS> <OBJECTS>")
 	set(CMAKE_C_ARCHIVE_FINISH   "<CMAKE_RANLIB> -no_warning_for_no_symbols -c <TARGET>")
 	set(CMAKE_CXX_ARCHIVE_FINISH "<CMAKE_RANLIB> -no_warning_for_no_symbols -c <TARGET>")
-    
-#    set(CMAKE_REQUIRED_FLAGS "-Werror=unguarded-availability-new ${CMAKE_REQUIRED_FLAGS}")
+
 endif()
