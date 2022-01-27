e29f131b8a9dc28e65439c2e62eba6cca58ed6c9
Maxim Samsonov <mamsonov@computer.org>
2022-01-27 22:24:59 +0300

    MacOS arm64 6

--- cmake/modules/FindJemalloc.cmake
+++ cmake/modules/FindJemalloc.cmake
@@ -14,32 +14,32 @@ find_package_handle_standard_args(Jemalloc DEFAULT_MSG Jemalloc_LIBRARIES
 mark_as_advanced(Jemalloc_LIBRARIES Jemalloc_INCLUDE_DIRS)
 
 if(Jemalloc_FOUND AND NOT (TARGET Jemalloc::Jemalloc))
-  set(CMAKE_REQUIRED_INCLUDES ${Jemalloc_INCLUDE_DIRS})
-  check_cxx_source_compiles(
-   "#include <jemalloc/jemalloc.h>
-    #if JEMALLOC_VERSION_MAJOR < 5
-    #error JEMALLOC_VERSION_MAJOR < 5
-    #endif
-    int main() {
-      return 0;
-    }"
-    JEMALLOC_VERSION_OK)
-  
-  if(NOT JEMALLOC_VERSION_OK)
-    def_ext_prj_g(JEMALLOC "5.2.1")
-    message(STATUS "Collecting jemalloc - " @${JEMALLOC_TAG}  " at " ${JEMALLOC_SOURCE_DIR})
-
-    ExternalProject_Add(${JEMALLOC_PRJ}
-      PREFIX "${DEPS}"
-      GIT_REPOSITORY "https://github.com/jemalloc/jemalloc.git"
-      GIT_TAG ${JEMALLOC_TAG}
-      UPDATE_COMMAND ""
-      CMAKE_ARGS ${CMAKE_ARGUMENTS}
-      SOURCE_DIR ${JEMALLOC_SOURCE_DIR}
-      BINARY_DIR ${JEMALLOC_BINARY_DIR}
-    )    
-
-  endif()
+#  set(CMAKE_REQUIRED_INCLUDES ${Jemalloc_INCLUDE_DIRS})
+#  check_cxx_source_compiles(
+#   "#include <jemalloc/jemalloc.h>
+#    #if JEMALLOC_VERSION_MAJOR < 5
+#    #error JEMALLOC_VERSION_MAJOR < 5
+#    #endif
+#    int main() {
+#      return 0;
+#    }"
+#    JEMALLOC_VERSION_OK)
+#  
+#  if(NOT JEMALLOC_VERSION_OK)
+#    def_ext_prj_g(JEMALLOC "5.2.1")
+#    message(STATUS "Collecting jemalloc - " @${JEMALLOC_TAG}  " at " ${JEMALLOC_SOURCE_DIR})
+#
+#    ExternalProject_Add(${JEMALLOC_PRJ}
+#      PREFIX "${DEPS}"
+#      GIT_REPOSITORY "https://github.com/jemalloc/jemalloc.git"
+#      GIT_TAG ${JEMALLOC_TAG}
+#      UPDATE_COMMAND ""
+#      CMAKE_ARGS ${CMAKE_ARGUMENTS}
+#      SOURCE_DIR ${JEMALLOC_SOURCE_DIR}
+#      BINARY_DIR ${JEMALLOC_BINARY_DIR}
+#    )    
+#
+#  endif()
   add_library(Jemalloc::Jemalloc UNKNOWN IMPORTED)
   set_target_properties(
     Jemalloc::Jemalloc
